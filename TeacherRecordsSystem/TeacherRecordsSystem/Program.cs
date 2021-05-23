5567b1d2a8cce6929c62c89eef36e21ff82e61f3
fidaa-alshaikh <fidaa.alshaikh@gmail.com>
2021-05-23 17:35:57 +0300

    append function has been added (storing record)

--- TeacherRecordsSystem/TeacherRecordsSystem/Program.cs
+++ TeacherRecordsSystem/TeacherRecordsSystem/Program.cs
@@ -21,11 +21,12 @@ namespace TeacherRecordsSystem
 
     class Program
     {
+        public static string fileLocation = "C:\\Users\\fedaa_1zoqddg\\OneDrive\\Desktop\\.NET-COURSE\\Projects\\Project1_Phase1\\.Net_Phase1_Project1\\TeachersRecord.txt";
         // Store Data Functions
         // Write data
         public static void WriteData()
         {
-            FileStream fs = new FileStream("C:\\Users\\fedaa_1zoqddg\\OneDrive\\Desktop\\.NET-COURSE\\Projects\\Project1_Phase1\\.Net_Phase1_Project1\\TeachersRecord.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            FileStream fs = new FileStream(fileLocation, FileMode.OpenOrCreate, FileAccess.Write);
             StreamWriter sw = new StreamWriter(fs);
             sw.WriteLine("Teachers Records");
             sw.WriteLine("ID \t Name \t Class \t Section");
@@ -33,12 +34,36 @@ namespace TeacherRecordsSystem
             sw.Close();
             fs.Close();
         }
- 
+        // Append data
+        public static void AppendData()
+        {
+            Console.Write("Please add teacher's ID:");
+            int id = Convert.ToInt32(Console.ReadLine());
+            // will try to make id auto increament !
+            Console.Write("Please add teacher's Name:");
+            string name = Console.ReadLine();
+            Console.Write("Please add teacher's Class Number:");
+            int classNum = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please add teacher's Section:");
+            int section = Convert.ToInt32(Console.ReadLine());
+
+            FileStream fs = new FileStream(fileLocation, FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+
+            Teacher t1 = new Teacher(id, name, classNum, section);
+            sw.WriteLine("{0} \t {1} \t {2} \t {3}", t1.id, t1.name, t1.classNum, t1.section);
+
+            sw.Close();
+            fs.Close();
+
+        }
         static void Main(string[] args)
         {
-            Teacher t1 = new Teacher(1, "Fidaa", 2, 20);
-            Console.WriteLine("ID: {0}, Name: {1}, Class Number: {2}, Sec: {3}", t1.id, t1.name, t1.classNum, t1.section);
+            //Teacher t1 = new Teacher(1, "Fidaa", 2, 20);
+           // Console.WriteLine("ID: {0}, Name: {1}, Class Number: {2}, Sec: {3}", t1.id, t1.name, t1.classNum, t1.section);
             WriteData();
+            AppendData();
+            Console.ReadKey();
         }
     }
 }
