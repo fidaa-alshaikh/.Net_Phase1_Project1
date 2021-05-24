a41076df2fa58e308be0ccf288759bf3ec253327
fidaa-alshaikh <fidaa.alshaikh@gmail.com>
2021-05-24 18:19:22 +0300

    update data function & switch cases have been added

--- TeacherRecordsSystem/TeacherRecordsSystem/Program.cs
+++ TeacherRecordsSystem/TeacherRecordsSystem/Program.cs
@@ -31,7 +31,7 @@ namespace TeacherRecordsSystem
             StreamWriter sw = new StreamWriter(fs);
             sw.WriteLine("Teachers Records");
             sw.WriteLine("ID \t Name \t Class \t Section");
-            
+
             sw.Close();
             fs.Close();
         }
@@ -39,10 +39,9 @@ namespace TeacherRecordsSystem
         // Append data
         public static void AppendData()
         {
-            // ##### will try to add try block to check the input !
             Console.Write("Please add teacher's ID:");
             int id = Convert.ToInt32(Console.ReadLine());
-            // ##### will try to make id auto increament !
+            // will try to make id auto increament !
             Console.Write("Please add teacher's Name:");
             string name = Console.ReadLine();
             Console.Write("Please add teacher's Class Number:");
@@ -58,29 +57,121 @@ namespace TeacherRecordsSystem
 
             sw.Close();
             fs.Close();
-
         }
 
         //Read data function (Print Record)
+        
         public static void ReadData()
         {
-            FileStream fs = new FileStream(fileLocation, FileMode.Open, FileAccess.Read);
-            StreamReader sr = new StreamReader(fs);
-            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+         FileStream fs = new FileStream(fileLocation, FileMode.Open, FileAccess.Read);
+         StreamReader sr= new StreamReader(fs);
+        sr.BaseStream.Seek(0, SeekOrigin.Begin);
             string str = sr.ReadLine();
             while (str != null)
             {
                 Console.WriteLine(str);
                 str = sr.ReadLine();
             }
+            fs.Close();
+            sr.Close();
         }
-        static void Main(string[] args)
+
+        // Update data function {NEED TO FIX}
+        public static void UpdateData(int id)
         {
-            //### need switch case!
-            //WriteData();
-            //AppendData();
-            ReadData();
-            Console.ReadKey();
+         FileStream fs3 = new FileStream(fileLocation, FileMode.Open, FileAccess.Read);
+         StreamReader sr3 = new StreamReader(fs3);
+            Console.Write("Please add teacher's Name:");
+            string name = Console.ReadLine();
+            Console.Write("Please add teacher's Class Number:");
+            int classNum = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please add teacher's Section:");
+            int section = Convert.ToInt32(Console.ReadLine());
+
+            Teacher t1 = new Teacher(id, name, classNum, section);
+            string updatedData = $"{t1.id} \t {t1.name} \t {t1.classNum} \t {t1.section}";
+            string[] lines;
+            using ( fs3)
+            {
+                using (sr3)
+                {
+                     lines = File.ReadAllLines(fileLocation);
+
+                    for (int i = 2; i < lines.Length; i++)
+                    {
+                        string[] split = lines[i].Split(',');
+                        foreach (var item in split)
+                        {
+                            if (Char.GetNumericValue(item[0]) == id)
+                            {
+                                lines[i] = updatedData;
+                            }
+                        }
+                    }
+
+                    foreach (var item in lines)
+                    {
+                        Console.WriteLine(item);
+                    }
+
+                }
+
+                //File.WriteAllLines(fileLocation, lines);
+                //using (FileStream fs = new FileStream(fileLocation, FileMode.OpenOrCreate, FileAccess.Write))
+                //{
+                //    using (StreamWriter sw = new StreamWriter(fs))
+                //    {
+                //        foreach (var item in lines)
+                //        {
+                //            sw.WriteLine(item);
+                //        }
+                //    }
+                //}
+            }
+        }
+
+
+                static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+                Console.WriteLine("(1) to enter new data \n" +
+               "(2) to update existing data \n" +
+               "(3) to display teachers record \n" +
+               "(4) to exit \n");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        WriteData();
+                        AppendData();
+                        break;
+                    case 2:
+                        ReadData();
+                        Console.Write("Please enter teacher's ID to update:");
+                        int id = Convert.ToInt32(Console.ReadLine());
+                        UpdateData(id);
+                        break;
+                    case 3:
+                        ReadData();
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input.");
+                        break;
+                }
+                if (choice == 4)
+                {
+                    break;
+                }
+
+             
+            }
+           
+            
+
         }
     }
 }
